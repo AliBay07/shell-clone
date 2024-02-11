70db446d67179f0528c562b091eda4b2a2e78dab
baydouna <ali.baydoun@etu.univ-grenoble-alpes.fr>
2024-02-11 19:47:32 +0100

    added more commands

--- shell.c
+++ shell.c
@@ -8,6 +8,8 @@
 #include <string.h>
 #include <dirent.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #define MAX_INPUT_SIZE 1024
 #define MAX_ARGUMENTS 1024
@@ -20,8 +22,6 @@ void change_directory(char *path) {
         if (path[0] == '/') {
             if (chdir(path) != 0) {
                 perror("cd");
-            } else {
-                strcpy(curdir, absolute_path);
             }
         } else {
 
@@ -105,10 +105,15 @@ void echo (char *tokens[MAX_ARGUMENTS + 2]) {
     char message[1024] = "";
     const char *fileName = NULL;
 
+    int toRead = 0;
+
     for (int i = 1; tokens[i] != NULL; ++i) {
         if (strcmp(tokens[i], ">") == 0) {
             fileName = tokens[i + 1];
             break;
+        } else if (strcmp(tokens[i], "<") == 0) {
+            fileName = tokens[i + 1];
+            toRead = 1;
         } else {
             strcat(message, tokens[i]);
             strcat(message, " ");
@@ -119,7 +124,7 @@ void echo (char *tokens[MAX_ARGUMENTS + 2]) {
 
     if (fileName == NULL) {
         printf("%s\n", message);
-    } else {
+    } else if (!toRead) {
         FILE *file = fopen(fileName, "w");
 
         if (file != NULL) {
@@ -128,6 +133,55 @@ void echo (char *tokens[MAX_ARGUMENTS + 2]) {
         } else {
             perror("Error opening file");
         }
+    } else {
+
+        int c;
+        FILE *file = fopen(fileName, "r");
+        if (file) {
+            while ((c = fgetc(file)) != EOF) {
+                printf("%c", c);
+            }
+            fclose(file);
+        }
+    }
+}
+
+void make_directory(char *path) {
+    char absolute_path[PATH_MAX];
+    char curdir[PATH_MAX];
+
+    if (path != NULL) {
+        if (path[0] == '/') {
+            if (mkdir(path, 0777) != 0) {
+                perror("mkdir");
+            } else {
+                strcpy(curdir, absolute_path);
+            }
+        } else {
+            if (getcwd(curdir, sizeof(curdir)) == NULL) {
+                perror("getcwd");
+                return;
+            }
+
+            if (snprintf(absolute_path, sizeof(absolute_path), "%s/%s", curdir, path) < 0) {
+                perror("snprintf");
+                return;
+            }
+
+            if (mkdir(absolute_path, 0777) != 0) {
+                perror("mkdir");
+                return;
+            }
+        }
+    }
+}
+
+void create_file(char *tokens[MAX_ARGUMENTS + 2]) {
+
+    for (int i = 1; tokens[i] != NULL; ++i) {
+        FILE *fptr;
+        fptr = fopen(tokens[i], "w");
+        fclose(fptr);
     }
 }
 
@@ -135,6 +189,8 @@ void handle_command(char *tokens[MAX_ARGUMENTS + 2]) {
 
     if (strcmp(tokens[0], "cd") == 0) {
         change_directory(tokens[1]);
+    } else if (strcmp(tokens[0], "mkdir") == 0) {
+        make_directory(tokens[1]);
     } else if (strcmp(tokens[0], "ls") == 0) {
         list_files();
     } else if (strcmp(tokens[0], "pwd") == 0) {
@@ -145,6 +201,8 @@ void handle_command(char *tokens[MAX_ARGUMENTS + 2]) {
         execute_file(tokens[0]);
     } else if (strcmp(tokens[0], "echo") == 0) {
         echo(tokens);
+    } else if (strcmp(tokens[0], "touch") == 0) {
+        create_file(tokens);
     } else {
         printf("Unknown command: %s\n", tokens[0]);
     }
